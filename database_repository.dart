627610af0c821147967c16eca29686490299ebcf
Miguele73 <cabusam.eu@gmail.com>
2025-03-28 14:19:44 +0100

    first_try

--- database_repository.dart
+++ database_repository.dart
@@ -1,21 +1,15 @@
 abstract class DatabaseRepository {
   void showSongs();
   void showArtists();
-  void showSongNotes();
-  void showSongChords();
-  void addSongNotes();
-  void addSongChords();
-  void deleteSongNotes();
-  void deleteSongChords();
+
   void addToFavorites();
   void removeFromFavorites();
   void showFavorites();
   void showLevels();
   void showChordList();
-  List<String> getSongNotes();
-  List<String> getSongChords();
-  List<String> getSongTitles();
-  List<String> getSongArtists();
-  List<String> getSongLevels();
-  List<String> getChordList();
+
+  List<Songs> getSongTitles();
+  List<Artists> getSongArtists();
+  List<Level> getSongLevels();
+  List<WholeChordList> getChordList();
 }
